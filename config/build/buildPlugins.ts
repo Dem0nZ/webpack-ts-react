17911173d7fb680434dbeb8572809374a9405200
Dem0nZ <cbpskov@gmail.com>
2022-09-27 16:52:55 +0300

    add HMR

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -11,10 +11,11 @@ export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPlugi
         new webpack.ProgressPlugin(),
         new MiniCssExtractPlugin({
             filename: 'css/[name].[contenthash:8].css',
-            chunkFilename: 'css/[name].[contenthash:8].css'
+            chunkFilename: 'css/[name].[contenthash:8].css',
         }),
         new webpack.DefinePlugin({
-            __IS_DEV__: JSON.stringify(isDev)
-        })
+            __IS_DEV__: JSON.stringify(isDev),
+        }),
+        new webpack.HotModuleReplacementPlugin(),
     ]
 }
