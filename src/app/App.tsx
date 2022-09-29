f4fc7e60651aa446884fe7a2f52133d134f72a5a
Dem0nZ <cbpskov@gmail.com>
2022-09-29 13:58:11 +0300

    add stylelint

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,12 +5,14 @@ import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 import { Suspense } from 'react';
+import { useTranslation } from 'react-i18next';
 
 function App() {
     const { theme } = useTheme();
+    const { t } = useTranslation();
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Suspense fallback="Loading...">
+            <Suspense fallback={t('loading')}>
                 <Navbar />
                 <div className="content-page">
                     <Sidebar />
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { useTranslation } from 'react-i18next';
+import { AppRoutes, RoutePath } from 'shared/config/routeConfig/routeConfig';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -13,8 +14,8 @@ export const Navbar = ({ className }: NavbarProps) => {
     return (
         <div className={classNames(cls.navbar, {}, [className])}>
             <div className={cls.links}>
-                <AppLink theme={AppLinkTheme.SECONDARY} to="/">{t('main')}</AppLink>
-                <AppLink theme={AppLinkTheme.SECONDARY} to="/about">{t('about')}</AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={RoutePath[AppRoutes.MAIN]}>{t('main')}</AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={RoutePath[AppRoutes.ABOUT]}>{t('about')}</AppLink>
             </div>
         </div>
     );
