75e06de0dddc8185d69cdbcda877405108cb8526
Dem0nZ <cbpskov@gmail.com>
2022-10-02 18:47:00 +0300

    add loaders

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,14 +5,12 @@ import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 import { Suspense } from 'react';
-import { useTranslation } from 'react-i18next';
 
 function App() {
     const { theme } = useTheme();
-    const { t } = useTranslation();
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Suspense fallback={t('loading')}>
+            <Suspense fallback="">
                 <Navbar />
                 <div className="content-page">
                     <Sidebar />
